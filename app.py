d15fd33d1021fcc194bc267267c0ee265d4e15ed
Be3yH4uk <vip.kisarov@mail.ru>
2021-03-07 01:22:12 +0300

    main > app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template, abort, redirect, url_for
+import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+main_index = 'Discord: xyz#7777<br>vk: vk.com/haqker'
+
+some_text = ""
 
 @app.route('/')
 def index():
-    return 'Discord: xyz#7777\nvk.com: vk.com/haqker'
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
